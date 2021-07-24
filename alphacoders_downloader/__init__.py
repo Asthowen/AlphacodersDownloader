855545c3fba38ad3df391fb21b25b78f34062101
Asthowen <raphael.pilinski@protonmail.com>
2021-07-24 12:17:21 +0200

    Add script to PyPI

---
+++ alphacoders_downloader/__init__.py
@@ -0,0 +1,4 @@
+__title__ = "AlphacodersDownloader"
+__version__ = "0.0.6"
+__author__ = "Asthowen"
+__license__ = "GNU v3.0"
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="AlphacodersDownloader",
+    version="0.0.6",
+    author="Asthowen",
+    author_email="contact@asthowen.fr",
+    license="GNU v3.0",
+    description="A script for download backgrounds on https://alphacoders.com/ written in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Asthowen/AlphacodersWallpaperDownloader",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": ['alphacoders_downloader = alphacoders_downloader.alphacoders_downloader:start']
+    },
+    python_requires='>= 3.6',
+    include_package_data=True,
+    install_requires=['aiohttp', 'aiofiles', 'bs4']
+)
