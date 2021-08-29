44a1ee8b4af3b78b7f87988a3d2fe8d38081d7e8
Asthowen <raphael.pilinski@protonmail.com>
2021-08-29 16:38:09 +0200

    Added package auto-generation for Debian

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
+with open('README.md', 'r', encoding='utf-8', errors='ignore') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="AlphacodersDownloader",
-    version="0.0.6",
-    author="Asthowen",
-    author_email="contact@asthowen.fr",
-    license="GNU v3.0",
-    description="A script for download backgrounds on https://alphacoders.com/ written in Python.",
+    name='AlphacodersDownloader',
+    version='0.0.7',
+    author='Asthowen',
+    author_email='contact@asthowen.fr',
+    maintainer='Asthowen',
+    maintainer_email='contact@asthowen.fr',
+    license='GNU v3.0',
+    description='A script for download backgrounds on https://alphacoders.com/ written in Python.',
+    description_content_type='text/markdown',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Asthowen/AlphacodersWallpaperDownloader",
+    long_description_content_type='text/markdown',
+    url='https://github.com/Asthowen/AlphacodersWallpaperDownloader',
     packages=setuptools.find_packages(),
     entry_points={
-        "console_scripts": ['alphacoders_downloader = alphacoders_downloader.alphacoders_downloader:start']
+        'console_scripts': ['alphacoders_downloader = alphacoders_downloader.alphacoders_downloader:start']
     },
     python_requires='>= 3.6',
     include_package_data=True,
-    install_requires=['aiohttp', 'aiofiles', 'bs4']
+    install_requires=['aiohttp', 'aiofiles', 'beautifulsoup4']
 )
