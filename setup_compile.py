e51ef511d2afa52996b62fe367bd8fbabb987bd9
Asthowen <raphael.pilinski@protonmail.com>
2021-11-28 15:37:35 +0100

    Release 0.1.2

--- setup_compile.py
+++ setup_compile.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='AlphacodersDownloader',
-    version='0.1.1',
+    version='0.1.2',
     author='Asthowen',
     author_email='contact@asthowen.fr',
     maintainer='Asthowen',
